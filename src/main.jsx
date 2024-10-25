6df1462214321344716fca7f7eab2b5a1a645d3e
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-10-25 03:05:41 +0300

    Buildind rusable star component - handle hover events

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import App from './App.jsx'
+// import './index.css'
 
-createRoot(document.getElementById('root')).render(
+import StarRating from "./components/StarRating";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={10} />
+  </StrictMode>
+);
